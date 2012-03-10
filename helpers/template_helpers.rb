b87fda3358504ee1dce8ffe3119d02b668f3910c
Phil Oye <philoye@philoye.com>
2012-03-10 21:05:07 +1100

    `hamstasche` helper to output a haml partial with mustache converted or not depending on whether a :mustache hash is passed in. This will let the same partial be used server side or client side.

--- helpers/template_helpers.rb
+++ helpers/template_helpers.rb
@@ -16,5 +16,18 @@ module Sinatra::Partials
     end
   end
 
+  def hamstache(template, *args)
+    template_array = template.to_s.split('/')
+    template = template_array[0..-2].join('/') + "/_#{template_array[-1]}"
+    options = args.last.is_a?(Hash) ? args.pop : {}
+    options.merge!(:layout => false)
+    mustache = options[:mustache] || nil
+    rendered = haml(:"#{template}", options)
+    if mustache
+      rendered = Mustache.render(rendered, mustache)
+    end
+    return rendered
+  end
+
 end
 
